1a7ea9c0c8433662954fbdf16ad9b1ddd1a80546
dafneolca <olca.dafne@gmail.com>
2019-07-21 23:14:46 +0200

    cleared up code and completed assignments

--- src/app/beers/beer-filter.pipe.ts
+++ src/app/beers/beer-filter.pipe.ts
@@ -3,13 +3,10 @@ import { PipeTransform, Pipe } from "@angular/core";
 @Pipe({ name: 'beerFilter' })
 
 export class beerFilterPipe implements PipeTransform {
-  transform(beers, userSearch, err) {
+  transform(beers, userSearch) {
     if (!beers || !userSearch) {
       return beers;
     }
-    // if (userSearch && !beers) {
-    //   return errorMessage;
-    // }
     return beers.filter(beers => beers.name.toLowerCase().indexOf(userSearch.toLowerCase()) !== -1)
   }
 }
--- src/app/beers/beer-list/beer-list.component.ts
+++ src/app/beers/beer-list/beer-list.component.ts
@@ -1,42 +1,31 @@
-import { Component, OnInit, ViewChildren } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { DrinksState } from '../store';
 import { Observable } from 'rxjs/index';
 import { getBeersSelector } from '../store/beers.selectors';
 import { fetchBeersListRequest } from '../store/beers.actions';
-
 import { Router } from '@angular/router';
-import { BeersEffects } from '../store/beers.effects';
-
-// import { InfiniteScroll } from 'ngx-infinite-scroll';
-
-// import { BehaviourSubject } from 'rxjs/BehaviorSubject';
 
 @Component({
   selector: 'app-beer-list',
   templateUrl: './beer-list.component.html',
   styleUrls: ['./beer-list.component.scss'],
-  // directives: [InfiniteScroll],
 })
-export class BeerListComponent implements OnInit {
 
-  @ViewChildren('beerFilterResult') createdItems;
+export class BeerListComponent implements OnInit {
 
   public beers$: Observable<any>;
 
   userSearch: string;
 
-  itemsPage: number = 3;
+  itemsPerPage: number = 5;
   onPage: number = 1;
-
-  // beer-not-found
 
   constructor(private store: Store<DrinksState>, private router: Router) { }
 
   ngOnInit() {
     this.store.dispatch(fetchBeersListRequest());
     this.beers$ = this.store.pipe(select(getBeersSelector));
-    console.log(this.beers$);
   }
 
   goToBeer(beer) {
@@ -45,20 +34,10 @@ export class BeerListComponent implements OnInit {
 
   pageChanged($event) {
     this.onPage = $event;
-    console.log(this.itemsPage)
-    console.log(this.createdItems)
   }
 
   clearFilter() {
     this.userSearch = "";
-  }
-
-  onScrollDown() {
-    console.log('scrolled down!!');
-  }
-
-  onScrollUp() {
-    console.log('scrolled up!!');
   }
 
 }
